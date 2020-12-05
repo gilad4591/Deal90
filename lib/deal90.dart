94e36eb7fee92e472da68ff2b9b19ec0c2fba034
ronmoshe12 <44731496+ronmoshe12@users.noreply.github.com>
2020-12-05 20:09:49 +0200

    add login page

---
+++ lib/deal90.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Deal90App extends StatelessWidget {
+  const Deal90App({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: Scaffold(),
+    );
+  }
+}
