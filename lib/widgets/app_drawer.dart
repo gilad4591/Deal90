3d80097dea6b1c2989641431d6aac24b3a39a774
Gilad Cohen <gilad4591@gmail.com>
2021-01-03 04:13:32 +0200

    logout manually and automatically

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,8 @@
+import 'package:finalproject/models/auth.dart';
 import 'package:flutter/material.dart';
 import '../screens/orders_screen.dart';
 import 'package:finalproject/screens/user_products_screen.dart';
+import 'package:provider/provider.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -30,11 +32,20 @@ class AppDrawer extends StatelessWidget {
           Divider(),
           ListTile(
               leading: Icon(Icons.edit),
-              title: Text('ManageProducts'),
+              title: Text('Manage Products'),
               onTap: () {
                 Navigator.of(context)
                     .pushReplacementNamed(UserProductScreen.routeName);
               }),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () {
+              Navigator.of(context).pop();
+              Provider.of<Auth>(context, listen: false).logout();
+            },
+          ),
         ],
       ),
     );
