14b9b3d200117846d7fefb08fea59963cdcbcffe
Gilad Cohen <gilad4591@gmail.com>
2021-02-28 04:50:02 +0200

    redirect after save profile

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:finalproject/providers/profile.dart';
 import 'package:finalproject/models/auth.dart';
+import 'package:finalproject/screens/product_overview_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
@@ -38,6 +39,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
     }
     //Navigator.of(context).pop();
     // ignore: deprecated_member_use
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ProductOveviewScreen()),
+    );
   }
 
   var currentLoggedInProfile = {
