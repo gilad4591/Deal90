f07f3b336fdbc0dd74fe1b383fc30429ab1f57c1
Gilad Cohen <gilad4591@gmail.com>
2021-03-18 21:03:28 +0200

    Show details of each deal creator

--- lib/providers/profile.dart
+++ lib/providers/profile.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Profile with ChangeNotifier {
-  // final GlobalKey<FormBuilderState> fbKey = GlobalKey<FormBuilderState>();
-
   String email;
   String name;
   String city;
@@ -18,34 +16,4 @@ class Profile with ChangeNotifier {
     this.userId,
     this.date,
   });
-  // CollectionReference users = Firestore.instance.collection('users');
-
-  // Profile getUserProfile(String userId) {
-  //   final db = Firestore.instance;
-  //   db.collection('users').document(userId).get().then(
-  //     (DocumentSnapshot documentSnapshot) {
-  //       // Get value of field date from document dashboard/totalVisitors
-  //       Profile initValues;
-  //       initValues.city = documentSnapshot.data['city'];
-  //       initValues.date = documentSnapshot.data['date'];
-  //       initValues.email = documentSnapshot.data['email'];
-  //       initValues.name = documentSnapshot.data['name'];
-  //       initValues.phone = documentSnapshot.data['phone'];
-  //       return initValues;
-  //     },
-  //   );
-  //   return null;
-  // }
-
-  // Future<void> updateUser(String userId) {
-  //   return users
-  //       .document(userId)
-  //       .updateData({'company': 'Stokes and Sons'})
-  //       .then((value) => print("User Updated"))
-  //       .catchError((error) => print("Failed to update user: $error"));
-  // }
-
-  Future<void> fetchAndSetProfile() async {
-    //final auth = Provider.of<Auth>(context, listen: false);
-  }
 }
--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,9 +1,53 @@
 import 'package:finalproject/providers/products.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ProductDetailsScreen extends StatelessWidget {
+class ProductDetailsScreen extends StatefulWidget {
   static const routeName = '/product-detail';
+
+  @override
+  _ProductDetailsScreenState createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  final creatorProfile = {
+    'city': '',
+    'date': '',
+    'email': '',
+    'name': '',
+    'phone': '',
+  };
+  var _isloading = true;
+
+  Future<void> loadProfile(String creatorId) async {
+    final db = Firestore.instance;
+    await db.collection('users').document(creatorId).get().then(
+      (DocumentSnapshot documentSnapshot) {
+        creatorProfile.update('city', (v) {
+          return documentSnapshot.data['city'];
+        });
+        creatorProfile.update('date', (v) {
+          return documentSnapshot.data['date'];
+        });
+        creatorProfile.update('name', (v) {
+          return documentSnapshot.data['name'];
+        });
+        creatorProfile.update('email', (v) {
+          return documentSnapshot.data['email'];
+        });
+        creatorProfile.update('phone', (v) {
+          return documentSnapshot.data['phone'];
+        });
+      },
+    );
+    if (_isloading) {
+      setState(() {
+        _isloading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String; //id
@@ -11,6 +55,10 @@ class ProductDetailsScreen extends StatelessWidget {
       context,
       listen: false,
     ).findById(productId);
+    loadProfile(loadedProduct.creatorId);
+
+    print(creatorProfile['name']);
+    // return creatorProfile['name'].length > 0 ?
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
@@ -57,7 +105,101 @@ class ProductDetailsScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
                 softWrap: true,
               ),
-            )
+            ),
+            SizedBox(height: 50),
+            !_isloading
+                ? CreatorDetailsButton(creatorProfile: creatorProfile)
+                : CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CreatorDetailsButton extends StatelessWidget {
+  const CreatorDetailsButton({
+    Key key,
+    @required this.creatorProfile,
+  }) : super(key: key);
+
+  final Map<String, String> creatorProfile;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+            side: BorderSide(color: Theme.of(context).buttonColor),
+          ),
+        ),
+      ),
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return CreatorDetailsDialog(creatorProfile: creatorProfile);
+          },
+        );
+      },
+      child: Text("Deal creator details"),
+    );
+  }
+}
+
+class CreatorDetailsDialog extends StatelessWidget {
+  const CreatorDetailsDialog({
+    Key key,
+    @required this.creatorProfile,
+  }) : super(key: key);
+
+  final Map<String, String> creatorProfile;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Container(
+        height: 100,
+        width: 100,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              right: 0.0,
+              top: 0.0,
+              child: InkResponse(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: CircleAvatar(
+                  child: Icon(Icons.close),
+                  backgroundColor: Colors.blue[900],
+                  radius: 15,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Name: ' + creatorProfile['name'],
+                  ),
+                  Text(
+                    'Email: ' + creatorProfile['email'],
+                  ),
+                  Text(
+                    'Phone: ' + creatorProfile['phone'],
+                  ),
+                  Text(
+                    'City: ' + creatorProfile['city'],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
