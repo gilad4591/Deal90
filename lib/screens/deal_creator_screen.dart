40343d4c2cab256a99447c319202170d19f9a4b9
ronmoshe12 <44731496+ronmoshe12@users.noreply.github.com>
2021-03-28 22:56:24 +0300

    working on deal creator screen

---
+++ lib/screens/deal_creator_screen.dart
@@ -0,0 +1,12 @@
+import 'package:finalproject/providers/products.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class DealCreatorScreen extends StatelessWidget {
+  static const routname = '/creator-card';
+
+  @override
+  Widget build(BuildContext context) {}
+}
